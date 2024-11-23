3f812cf2b56701fc96ff3bbd7e88319c59bb7934
Евгений <info-best@bk.ru>
2024-11-23 08:06:55 +0300

    second1

--- Module_13_1.py
+++ Module_13_1.py
@@ -1,20 +1,20 @@
 import asyncio
-
+ball = 5
 
 # Асинхронная функция, имитирующая участие силача в соревновании
 async def start_strongman(name, power):
     print(f"Силач {name} начал соревнования.")
 
     # Задержка, обратно пропорциональная силе
-    delay = 5 / power  # Чем больше сила, тем меньше задержка
+    delay = ball / power  # Чем больше сила, тем меньше задержка
 
     # Подъем 5 шаров
-    for i in range(1, 6):  # 1 до 5 включительно
+    for i in range(ball):  # 1 до 5 включительно
         await asyncio.sleep(delay)  # Задержка
-        print(f"Силач {name} поднял {i} шар.")
-
-    # Завершение соревнований
+        print(f"Силач {name} поднял {i + 1} шар.")
+    await asyncio.sleep(delay)
     print(f"Силач {name} закончил соревнования.")
+
 
 
 # Асинхронная функция для запуска турнира
@@ -25,9 +25,12 @@ async def start_tournament():
     task3 = asyncio.create_task(start_strongman("Apollon", 5))
 
     # Ожидаем завершения всех задач
-    await asyncio.gather(task1, task2, task3)
+    await task1
+    await task2
+    await task3
+
+    # Завершение соревнований
 
 
 # Запуск турнира
-if __name__ == "__main__":
-    asyncio.run(start_tournament())
+asyncio.run(start_tournament())
